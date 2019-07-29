490470abbcdfef989bb1ecf743d7ce1d374180ff
R0brt99 <53383568+R0brt99@users.noreply.github.com>
2019-07-29 17:15:17 -0400

    Added Team Select Feature
    Returns Pairings of Teams for Turf Wars

--- cogs/general.py
+++ cogs/general.py
@@ -1,3 +1,4 @@
+import random
 import discord
 from discord.ext import commands
 
@@ -11,14 +12,15 @@ class General(commands.Cog):
         await ctx.send(f'Pong! {round(self.client.latency * 1000)} ms!')
         print(ctx.channel)
 
-"""
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        channel = after.guild
-        if str(before.status) == 'offline':
-            await channel.send('{} has Returned!'.format(after.name))
-            print('{} has returned'.format(after.name))
-"""
+    @commands.command()
+    async def ts(self, ctx):
+        number = random.randint(1, 3)
+        if number == 1:
+            await ctx.send('R0brt & DiamondTrident vs. Dolphino & Garrett Gaming')
+        elif number == 2:
+            await ctx.send('DiamondTrident & Dolphino vs. R0brt & Garrett Gaming')
+        elif number == 3:
+            await ctx.send('Garrett Gaming & DiamondTrident vs. Dolphino & R0brt')
 
 def setup(client):
     client.add_cog(General(client))
